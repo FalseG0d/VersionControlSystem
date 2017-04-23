3e755f0de6baf3422ead389e597374813ec5ae72
Paras Bhattrai <sivssdn@gmail.com>
2017-04-23 11:04:32 +0530

    login updated

--- login.py
+++ login.py
@@ -1,18 +1,13 @@
-import sys
-
-
-def login():
-    if (len(sys.argv) < 2):
-        print("Syntax of the program: python login.py your_user_name")
-    else:
+def login(userName):
         users = open("user.txt", "r")
         foundUser = False
         for line in users:
-            if (line.strip('\t\n') == sys.argv[1]):
+            if (line.strip('\t\n') == userName):
                 foundUser = True
                 break
 
         print(foundUser)
+        return foundUser
 
 
-login()
+login("Paras")
